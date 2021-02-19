0763c48477dd6df1d8cc402cc9c4ab7533dcf995
BlackKnight683 <75422863+BlackKnight683@users.noreply.github.com>
2021-02-19 18:56:49 +0530

    Update close.js

--- commands/ticket/close.js
+++ commands/ticket/close.js
@@ -16,23 +16,6 @@ module.exports = {
 			const member = message.guild.members.cache.get(message.channel.name.split('ticket-').join(''));
 			if(message.member.hasPermission('ADMINISTRATOR') || message.channel.name === `ticket-${message.author.id}`) {
 				message.channel.messages.fetch().then(async (messages) => {
-					const output = messages.array().reverse().map(m => `${new Date(m.createdAt).toLocaleString('en-US')} - ${m.author.tag}: ${m.attachments.size > 0 ? m.attachments.first().proxyURL : m.content}`).join('\n');
-
-					let response;
-					try {
-						response = await fetch(url, { method: 'POST', body: output, headers: { 'Content-Type': 'text/plain' } });
-					}
-					catch(e) {
-						console.log(e);
-						return message.channel.send('An error occurred, please try again!');
-					}
-
-					const { key } = await response.json();
-					const embed = new MessageEmbed()
-						.setDescription(`[\`📄 View\`](https://hastebin.com/${key}.js)`)
-						.setColor('GREEN');
-					member.send('Here is a transcript of your ticket, please click the link below to vew the transcript', embed);
-				}).then(() => {
 					try {
 						message.channel.updateOverwrite(member.user, {
 							VIEW_CHANNEL: false,
